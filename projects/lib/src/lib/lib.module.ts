8a850a866e9005c04d33271fd7ae80f9e6a2b863
Mukesh Rathore <mukesh.rathore86@gmail.com>
2020-04-01 19:04:05 -0400

    removing table component to now until all the setup is done

--- projects/lib/src/lib/lib.module.ts
+++ projects/lib/src/lib/lib.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { LibComponent } from './lib.component';
-import { TableComponent } from './components/table/table.component';
-import { ClientPaginatedTableComponent } from './components/table/client-paginated-table/client-paginated-table.component';
-import { ServerPaginatedTableComponent } from './components/table/server-paginated-table/server-paginated-table.component';
-
-
 
 @NgModule({
-  declarations: [LibComponent, TableComponent, ClientPaginatedTableComponent, ServerPaginatedTableComponent],
+  declarations: [LibComponent],
   imports: [
   ],
   exports: [LibComponent]
