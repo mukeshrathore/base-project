caedb42a7acd13cd747b7e5c10cad0471a77eb89
Mukesh Rathore <mukesh.rathore86@gmail.com>
2020-04-20 17:37:43 -0400

    adding route-error-handler components in core module

---
+++ projects/base-app/src/app/core/route-error-handlers/session-timeout/session-timeout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-session-timeout',
+  templateUrl: './session-timeout.component.html',
+  styleUrls: ['./session-timeout.component.scss']
+})
+export class SessionTimeoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
