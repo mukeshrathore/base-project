e2f2369d323802cdf7385ba7f33674b6ca14979e
Mukesh Rathore <mukesh.rathore86@gmail.com>
2020-04-20 18:00:56 -0400

    adding button and table components to lib

--- projects/lib/src/lib/lib.module.ts
+++ projects/lib/src/lib/lib.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { LibComponent } from './lib.component';
+import { BaseTableComponent } from './components/base-table/base-table.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [LibComponent],
+  declarations: [LibComponent, BaseTableComponent, ButtonComponent, TableComponent],
   imports: [
   ],
   exports: [LibComponent]
