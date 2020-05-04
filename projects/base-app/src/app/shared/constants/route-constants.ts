615b5e43f321c6f8922d36533b0d65583cdc72fe
Mukesh Rathore <mukesh.rathore86@gmail.com>
2020-05-04 18:43:26 -0400

    creating shared module and adding constants

---
+++ projects/base-app/src/app/shared/constants/route-constants.ts
@@ -0,0 +1,8 @@
+export const ROUTE_CONSTANTS = {
+    // General URLs
+    DEFAULT_ROUTE: '',
+    PATH_MATCH_FULL: 'full',
+    WILDCARD_ROUTE: '**',
+    ACCESS_DENIED_ROUTE: 'access-denied',
+    SESSION_TIMEOUT: 'session-timeout',
+};
