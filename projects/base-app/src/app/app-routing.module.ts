6a2da6c4e6c5d7eed725a3aa202a2ce93fad8405
Mukesh Rathore <mukesh.rathore86@gmail.com>
2020-05-04 18:44:21 -0400

    default routing

--- projects/base-app/src/app/app-routing.module.ts
+++ projects/base-app/src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ROUTE_CONSTANTS } from './shared/constants/route-constants';
+import { AppComponent } from './app.component';
+import { AccessDeniedComponent } from './core/route-error-handlers/access-denied/access-denied.component';
+import { SessionTimeoutComponent } from './core/route-error-handlers/session-timeout/session-timeout.component';
+import { PageNotFoundComponent } from './core/route-error-handlers/page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ROUTE_CONSTANTS.DEFAULT_ROUTE,
+    redirectTo: ROUTE_CONSTANTS.DEFAULT_ROUTE,
+    pathMatch: ROUTE_CONSTANTS.PATH_MATCH_FULL
+  },
+  {
+    path: ROUTE_CONSTANTS.DEFAULT_ROUTE,
+    component: AppComponent
+  },
+  {
+    path: ROUTE_CONSTANTS.ACCESS_DENIED_ROUTE,
+    component: AccessDeniedComponent
+  },
+  {
+    path: ROUTE_CONSTANTS.SESSION_TIMEOUT,
+    component: SessionTimeoutComponent
+  },
+  {
+    path: ROUTE_CONSTANTS.WILDCARD_ROUTE,
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
