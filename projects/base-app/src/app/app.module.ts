5e8e57ee8b9d1ee3af3b2e67c314cc4ca7e599ba
Mukesh Rathore <mukesh.rathore86@gmail.com>
2020-05-06 02:29:43 -0400

    adding formsModule and ReactiveFormsModule for application wide  use

--- projects/base-app/src/app/app.module.ts
+++ projects/base-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule, AppLibModule } from 'app-lib';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,8 @@ import { MaterialModule, AppLibModule } from 'app-lib';
     BrowserModule,
     AppLibModule,
     MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
